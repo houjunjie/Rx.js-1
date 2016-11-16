9c56efe21c6ee5aeb8f3d0c767781ea04bfaee96
L3ve <370157506@qq.com>
2016-11-16 17:17:03 +0800

    兼容rxjs模块加载

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,24 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import async from 'rollup-plugin-async';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
 import path from 'path';
 
 export default {
     entry: 'src/test.js',
     dest: 'dist/bundle.js',
     format: 'umd',
-    plugins: [json(),async(), babel({
-        exclude: 'node_modules/**'
-    })]
+    plugins: [
+        json(),
+        async(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        nodeResolve({
+            jsnext: true
+        }),
+        commonjs()
+    ]
 };
